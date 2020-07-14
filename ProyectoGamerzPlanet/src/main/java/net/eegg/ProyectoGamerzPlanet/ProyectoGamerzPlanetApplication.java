a259954422dc2c4fc9651ca90ef1b2871f0d9e55
Esther-Carrion <68031785+Esther-Carrion@users.noreply.github.com>
2020-07-14 16:12:13 -0500

    Add files via upload

---
+++ ProyectoGamerzPlanet/src/main/java/net/eegg/ProyectoGamerzPlanet/ProyectoGamerzPlanetApplication.java
@@ -0,0 +1,13 @@
+package net.eegg.ProyectoGamerzPlanet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoGamerzPlanetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoGamerzPlanetApplication.class, args);
+	}
+
+}
---
+++ ProyectoGamerzPlanet/src/test/java/net/eegg/ProyectoGamerzPlanet/ProyectoGamerzPlanetApplicationTests.java
@@ -0,0 +1,13 @@
+package net.eegg.ProyectoGamerzPlanet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoGamerzPlanetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
